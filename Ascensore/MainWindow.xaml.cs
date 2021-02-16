b95ea7d8951e5024b0969dc6330e496250d221c3
SimoneGramellini <simone.gramellini.stud@ispascalcomandini.it>
2021-02-16 12:00:48 +0100

    Aggiunta logica spostamento cabina
    Aggiunta la logica che permette alla cabina di spostarsi a seconda del bottone premuto

--- Ascensore/MainWindow.xaml.cs
+++ Ascensore/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace Ascensore
 {
@@ -20,39 +21,94 @@ namespace Ascensore
     /// </summary>
     public partial class MainWindow : Window
     {
+        const int ALTEZZA_PIANO_1 = 665;
+        const int ALTEZZA_PIANO_2 = 543;
+        const int ALTEZZA_PIANO_3 = 410;
+        const int ALTEZZA_PIANO_4 = 273;
+        const int ALTEZZA_PIANO_5 = 143;
+        const int ALTEZZA_PIANO_6 = 22;
+        int altezzaDaRaggiungere = 0;
+        bool salire = false;
+        int posizioneCabinaTop = 0;
         public MainWindow()
         {
             InitializeComponent();
+            cabina.Margin = new Thickness(28, ALTEZZA_PIANO_3, 0, 0); //posizione di partenza
         }
 
+        public void MuoviAscensore()
+        {
+            if (!salire) //devo scendere
+            {
+                for (int i = posizioneCabinaTop; i < altezzaDaRaggiungere; i += 2)
+                {
+                    Thread.Sleep(TimeSpan.FromSeconds(0.01));
+                    this.Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        cabina.Margin = new Thickness(28, i, 0, 0);
+                    }));
+                }
+            }
+            else //salgo
+            {
+                for (int i = posizioneCabinaTop; i > altezzaDaRaggiungere; i -= 2)
+                {
+                    Thread.Sleep(TimeSpan.FromSeconds(0.01));
+                    this.Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        cabina.Margin = new Thickness(28, i, 0, 0);
+                    }));
+                }
+            }
+        }
+        public void GestisciPiano()
+        {
+            if ((int)cabina.Margin.Top <= altezzaDaRaggiungere)
+            {
+                salire = false;
+            }
+            else
+            {
+                salire = true;
+            }
+            posizioneCabinaTop = (int)cabina.Margin.Top;
+            Thread t = new Thread(new ThreadStart(MuoviAscensore));
+            t.Start();
+        }
         private void btn1_Click(object sender, RoutedEventArgs e)
         {
-
+            altezzaDaRaggiungere = ALTEZZA_PIANO_1;
+            GestisciPiano();
         }
 
         private void btn2_Click(object sender, RoutedEventArgs e)
         {
-
+            altezzaDaRaggiungere = ALTEZZA_PIANO_2;
+            GestisciPiano();
         }
 
         private void btn3_Click(object sender, RoutedEventArgs e)
         {
-
+            altezzaDaRaggiungere = ALTEZZA_PIANO_3;
+            GestisciPiano();
         }
 
         private void btn4_Click(object sender, RoutedEventArgs e)
         {
-
+            altezzaDaRaggiungere = ALTEZZA_PIANO_4;
+            GestisciPiano();
         }
 
         private void btn5_Click(object sender, RoutedEventArgs e)
         {
-
+            altezzaDaRaggiungere = ALTEZZA_PIANO_5;
+            GestisciPiano();
         }
 
         private void btn6_Click(object sender, RoutedEventArgs e)
         {
-
+            altezzaDaRaggiungere = ALTEZZA_PIANO_6;
+            GestisciPiano();
         }
     }
 }
